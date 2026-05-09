d654f79261652de41a61a2674d8ad1d5e6bc445c
Vidz2810 <arisasrividya@gmail.com>
2026-05-09 12:44:07 +0530

    fix tailwind production

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,14 @@ import react from '@vitejs/plugin-react'
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 import netlify from '@netlify/vite-plugin-tanstack-start'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [
     tsconfigPaths(),
     tanstackStart(),
     react(),
+    tailwindcss(),
     netlify(),
   ],
 })
