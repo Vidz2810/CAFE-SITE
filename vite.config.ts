5f15acfc36e8664909d544b08074d52ddce1f742
Vidz2810 <arisasrividya@gmail.com>
2026-05-09 12:33:43 +0530

    add netlify plugin

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
+import netlify from '@netlify/vite-plugin-tanstack-start'
 
 export default defineConfig({
   plugins: [
     tsconfigPaths(),
     tanstackStart(),
     react(),
+    netlify(),
   ],
 })
